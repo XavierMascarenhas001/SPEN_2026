a7cd1a8afb27bbbaa2dd65e2b5c0a79acad00e07
XavierMascarenhas001 <xavier.mascarenhas@gaeltecutilities.com>
2026-02-13 10:04:58 +0000

    Update Gaeltec2.0.py

--- Gaeltec2.0.py
+++ Gaeltec2.0.py
@@ -650,6 +650,7 @@ conductor_2_keys = {
 
 equipment_keys = {
     "Noja": "Noja",
+    "0.5 kVa Tx for Noja": "0.5 kVa Tx for Noja",
     "11kV PMSW (Soule)": "11kV PMSW (Soule)",
     "11kv ABSW Hookstick Standard": "11kv ABSW Hookstick Standard",
     "11kv ABSW Hookstick Spring loaded mech": "11kv ABSW Hookstick Spring loaded mech",
@@ -689,7 +690,6 @@ equipment_keys = {
     "11KV OHL FUSE ELEMENT DJP-TYPE 30A": "11KV OHL FUSE ELEMENT DJP-TYPE 30A",
     "11KV OHL FUSE ELEMENT DJP-TYPE 40A": "11KV OHL FUSE ELEMENT DJP-TYPE 40A",
     "11KV OHL FUSE ELEMENT DJP-TYPE 50A": "11KV OHL FUSE ELEMENT DJP-TYPE 50A",
-    "0.5 kVa Tx for Noja": "0.5 kVa Tx for Noja",
 }
 
 
@@ -700,21 +700,22 @@ summary_items = [
     "Recover single pole, up to and including 15 metres in height, and reinstate, all ground conditions",
     "Recover 'A' / 'H' pole, up to and including 15 metres in height, and reinstate, all ground conditions",
     "Erect 11kV/33kV ABSW.",
-    "Erect 11kV Remote Controlled Switch Disconnector ( Soule Auguste ) or Auto Reclosure unit c/w VT, Aerial, RTU & umbilical cable",
+    "Remove 11kV/33kV ABSW",
+    "Noja"
+    "0.5 kVa Tx for Noja"
+    "11kV PMSW (Soule)"
+    "Remove Auto Reclosure",
     "Erect pole mounted transformer up to 100kVA 1.ph",
     "Erect pole mounted transformer up to 200kVA 3.p.h",
     "Remove pole mounted transformer",
     "Remove platform mounted or 'H' pole mounted transformer",
-    "Remove 11kV/33kV ABSW",
-    "Remove Auto Reclosure",
     "Install bare conductor, run out, sag, terminate, bind in and connect jumpers; <100mm²",
     "Install bare conductor, run out, sag, terminate, bind in and connect jumpers; >=100mm² <200mm²",
     "Install conductor, run out, sag, terminate, clamp in and connect jumpers; 2c + Earth",
     "Install conductor, run out, sag, terminate, clamp in and connect jumpers; 4c + Earth",
     "Install service span including connection to mainline & building / structure",
-    "Remove 1.ph or 3.ph HV fuses",
     "Erect 3.ph fuse units at single tee off pole or in line pole"
-    
+    "Remove 1.ph or 3.ph HV fuses",    
 ]
 
 categories = [
